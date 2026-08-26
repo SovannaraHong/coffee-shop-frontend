a8fa94c45a3ef40881c5bac4da5d529217f98d6b
SovannaraHong <rapewpew372@gmail.com>
2026-08-26 21:11:06 +0700

    config-api

--- projects/admin-web/src/app/app.config.ts
+++ projects/admin-web/src/app/app.config.ts
@@ -1,7 +1,18 @@
+import { API_URL } from './../../../../libs/api/api-config';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { environment } from './environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    provideHttpClient(),
+    {
+      provide: API_URL,
+      useValue: environment.apiUrl,
+    },
+  ],
 };
